0b528f55837c5bd45f2451c4d627b1aab8bef8d2
Yoshinao Inoguchi <apple@inoguchi.com>
2024-11-23 01:52:14 +0900

    Minor update

--- yyChat/Program.cs
+++ yyChat/Program.cs
@@ -7,13 +7,19 @@
             var xBuilder = WebApplication.CreateBuilder (args);
 
             xBuilder.Services.Configure <RouteOptions> (x => x.LowercaseUrls = true);
+
+            // Add services to the container.
             xBuilder.Services.AddRazorPages ();
 
             var xApp = xBuilder.Build ();
 
+            // Configure the HTTP request pipeline.
             if (xApp.Environment.IsDevelopment () == false)
             {
+                // The default HSTS value is 30 days.
+                // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 xApp.UseHsts ();
+
                 xApp.UseExceptionHandler ("/Error");
             }
 
